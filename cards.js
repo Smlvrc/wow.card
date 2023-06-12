0947db98b284cded3fb5e77310a19384ae0f9585
Smlvrc <suleymanovasema50@gmail.com>
2023-06-12 05:52:17 -0700

    first commit

---
+++ cards.js
@@ -0,0 +1,16 @@
+
+        // slider
+        var swiper = new Swiper(".mySwiper", {
+            scrollbar: {
+              el: ".swiper-scrollbar",
+              hide: true,
+            },
+          });
+          function startAutoplay() {
+            autoplayInterval = setInterval(nextSlide, autoplayDelay);
+          }
+
+
+
+        //   max min
+
